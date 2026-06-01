d76577e5b9a8bfc438c274f2b1801ce5d4c460c7
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-06-01 06:47:42 +0530

    Optimize DB connection

--- index.js
+++ index.js
@@ -1,46 +1,66 @@
 const express = require("express");
+
 const app = express();
+
 const cors = require("cors");
+
 const dotenv = require("dotenv");
+
 dotenv.config();
+
 const { initializeDatabase } = require("./config/db");
 
+
+// CONNECT DATABASE ONLY ONCE
+initializeDatabase();
+
+
+// ROUTES
 const projectRoutes = require("./routes/projectRoutes");
+
 const teamRoutes = require("./routes/teamRoutes");
+
 const taskRoutes = require("./routes/taskRoutes");
+
 const userRoutes = require("./routes/userRoutes");
+
 const authRoutes = require("./routes/authRoutes");
 
+
+// MIDDLEWARE
 app.use(cors());
+
 app.use(express.json());
 
-app.use("/api", async (req, res, next) => {
-  try {
-    await initializeDatabase();
-    next();
-  } catch (error) {
-    res.status(500).json({
-      message: "Database connection failed",
-      error: error.message,
-    });
-  }
-});
 
+// API ROUTES
 app.use("/api/projects", projectRoutes);
+
 app.use("/api/auth", authRoutes);
+
 app.use("/api/users", userRoutes);
+
 app.use("/api/tasks", taskRoutes);
+
 app.use("/api/teams", teamRoutes);
 
+
+// HOME ROUTE
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
+// LOCAL DEVELOPMENT
 if (require.main === module) {
+
   const PORT = process.env.PORT || 5000;
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 }
+
+
 
 module.exports = app;
